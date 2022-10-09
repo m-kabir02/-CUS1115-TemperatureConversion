d375aab21fad77d24c78e47b6dc002ff01df6362
Mosammad Kabir <97767575+m-kabir02@users.noreply.github.com>
2022-10-08 22:58:37 -0400

    Add files via upload

---
+++ TemperatureConversion/src/TemperatureConversion.java
@@ -0,0 +1,9 @@
+public class TemperatureConversion {
+    public static void main(String[] args) {
+
+        for(int i=0;i<=100;i++)
+        {
+            System.out.println(i+" degrees Farenheit is "+(5*(i-32)/9)+" in degrees Celcius.");
+        }
+    }
+}
